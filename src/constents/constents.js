cfd360c366fead119a770c565e3da94684e0519b
Ajith R <ajith8593935904@gmail.com>
2024-04-15 09:15:17 +0530

    netflix 3

---
+++ src/constents/constents.js
@@ -0,0 +1,3 @@
+export const baseURL = 'https://api.themoviedb.org/3';
+export const API_KEY = '3dd1aaa05a0beee725d3c9e2777b2428';
+export const imageURL = 'https://image.tmdb.org/t/p/original'
---
+++ src/urls.js
@@ -0,0 +1,6 @@
+import { API_KEY } from "./constents/constents.js";
+
+export const originals = `discover/tv?api_key=${API_KEY}&with_networ=ks213`;
+export const actions = `discover/movie?api_key=${API_KEY}&with_genres=28`;
+export const ComedyMovies = `discover/movie?api_key=${API_KEY}&with_genres=35`;
+export const HorrorMovies = `discover/movie?api_key=${API_KEY}&with_genres=27`;
